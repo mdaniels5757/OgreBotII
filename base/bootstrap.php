cd4bb5606933743910a2bea241880a7219c32025
Magog <magog.the.ogre@gmail.com>
2017-07-04 21:40:31 -0400

    renaming local_io => Local_Io

--- base/bootstrap.php
+++ base/bootstrap.php
@@ -9,7 +9,7 @@ if (defined('HHVM_VERSION')) {
 
 require_once "String_Utils.php";
 require_once "wiki_string_utils.php";
-require_once "local_io.php";
+require_once "Local_Io.php";
 require_once "remote_io.php";
 require_once __DIR__ . "/../REL1_0/Init.php";
 
