14aed15644774a62671627dd9efd10a541a1ef02
Magog <magog.the.ogre@gmail.com>
2018-03-03 00:14:24 -0500

    again

--- fileinfo/Fileinfo_Web_Logic.php
+++ fileinfo/Fileinfo_Web_Logic.php
@@ -228,18 +228,18 @@ class Fileinfo_Web_Logic {
 			$this->edit_file_local_link = wikilink($title, $this->project, true, false, 
 				$edit_local_text, "", false, "edit");
 			
-			$dest = $commons_listed ? $commons_listed : $src;
-			$dest_sanitized = sanitize($dest);
+			$dest_title = $commons_listed ? $commons_listed : $title;
+			$dest_sanitized = $commons_listed ? sanitize($commons_listed) : $this->src_sanitized;
 			$view_commons_text = replace_named_variables($this->messages["view_commons"], 
 				array("file" => $dest_sanitized));
 			$edit_commons_text = replace_named_variables($this->messages["edit_commons"], 
 				array("file" => $dest_sanitized));
 			
-			$this->view_file_commons_link = wikilink($dest, "commons.wikimedia", true, false,
-				$view_commons_text, "");
-			$this->edit_file_commons_link = wikilink($dest, "commons.wikimedia", true, false,
-				$edit_commons_text, "", false, "edit");
-				
+			$this->view_file_commons_link = wikilink($dest_title, "commons.wikimedia", true, 
+				false, $view_commons_text, "");
+			$this->edit_file_commons_link = wikilink($dest_title, "commons.wikimedia", true, 
+				false, $edit_commons_text, "", false, "edit");
+			
 			if ($information) {
 				$options = 0;
 				if ($authordate) {
