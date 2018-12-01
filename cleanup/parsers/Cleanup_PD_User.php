a8fc219d9eb3b50435785846386ca6d9247bd752
Magog <magog.the.ogre@gmail.com>
2018-12-01 12:35:46 -0500

    PD-user-az doesn't exist anymore

--- cleanup/parsers/Cleanup_PD_User.php
+++ cleanup/parsers/Cleanup_PD_User.php
@@ -14,12 +14,12 @@ class Cleanup_PD_User implements Cleanup_Module {
 	public function cleanup(Cleanup_Instance $ci) {
 		// {{PD-user-w|en|wikipedia}} -> {{PD-user-en}}
 		$ci->preg_replace(
-			"/\{\{\s*PD\-user\-w\s*\|\s*(als|ar|az|bg|ca|cs|da|de|en|es|fi|fr|he|hi|hr|hu|it|ja|ka" .
+			"/\{\{\s*PD\-user\-w\s*\|\s*(als|ar|bg|ca|cs|da|de|en|es|fi|fr|he|hi|hr|hu|it|ja|ka" .
 				 "|lt|lv|ml|nl|nn|no|pl|pt|ro|ru|simple|sk|sl|sv|th|uk|vls|zh)\s*\|\s*wikipedia\s*\|" .
 				 "\s*(.+?)\s*\}\}/u", "{{PD-user-$1|$2}}", false);
 		$ci->preg_replace(
 			"/\{\{\s*PD\-user\-w\s*\|\s*project\s*\=\s*wikipedia\s*\|\s*language\s*\=\s*" .
-				 "(als|ar|az|bg|ca|cs|da|de|en|es|fi|fr|he|hi|hr|hu|it|ja|ka|lt|lv|ml|nl|nn|no|pl|pt|ro|" .
+				 "(als|ar|bg|ca|cs|da|de|en|es|fi|fr|he|hi|hr|hu|it|ja|ka|lt|lv|ml|nl|nn|no|pl|pt|ro|" .
 				 "ru|simple|sk|sl|sv|th|uk|vls|zh)\s*\|\s*user\s*\=\s*([^\[\]\{\}\|]+)\}\}/u", 
 				"{{PD-user-$1|1=$2}}", false);
 		$ci->preg_replace(
