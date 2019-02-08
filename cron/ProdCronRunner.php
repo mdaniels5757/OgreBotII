cc6ba1f66b6e3ab7576a53d95a867ad07ad7b198
Magog <magog.the.ogre@gmail.com>
2019-02-07 19:44:29 -0500

    update cron job to new grid engine

--- cron/ProdCronRunner.php
+++ cron/ProdCronRunner.php
@@ -11,7 +11,7 @@ class ProdCronRunner extends CronRunner {
 		$command   = $this->runConfig->getCommand();
 		$args      = $this->runConfig->getArgs();
 		$timeout   = $this->runConfig->getTimeout();
-		$vm		   = $this->runConfig->getHhvm() ? "hhvm" : "php";
+		$vm		   = "php"; //$this->runConfig->getHhvm() ? "hhvm" : "php";
 
 		return "cd ".BASE_DIRECTORY."/$directory; timeout $timeout $vm $command.php $args >> ".LOG_DIRECTORY."/${command}_errors.log";
 	}
