44e04dec4715dc59b1fb71e52fb59a82bef7f0bf
Magog <magog.the.ogre@gmail.com>
2019-02-09 00:11:09 -0500

    partial revert (NodeJS not installed on Kubernetes)

--- base/Local_Io.php
+++ base/Local_Io.php
@@ -188,29 +188,6 @@ function ogrebotExec($command) {
 
 /**
  *
- * @param string $command
- * @return string
- * @throws Exception
- */
-function ogrebotExecWithOutput(string $command): string {
-	global $logger;
-	
-	$logger->info("Running:\n$command");
-	
-	exec($command, $output, $return_var);
-	
-	$logger->info(" ...done");
-	
-	if ($return_var !== 0) {
-		throw new Exception("Command $command FAILED. Return status: $return_var.");
-	}
-	
-	return join("\n", $output);
-}
-
-
-/**
- *
  * @param string $prompt        	
  * @throws IllegalStateException
  */
--- category-files/Category_Files_Log_Entry.php
+++ category-files/Category_Files_Log_Entry.php
@@ -52,11 +52,46 @@ class Category_Files_Log_Entry {
 	
 	/**
 	 * 
-	 * @return string[][]
+	 * @param string $filename
+	 * @return string[]
 	 */
-	public static function get_all_gallery_names(): array {
-		$output = ogrebotExecWithOutput("node " . BASE_DIRECTORY . "/js/exe/get-gallery-names.js");
-		return json_decode($output, true);
+	public static function get_all_gallery_names_by_file($filename) {
+		$content = file_get_contents_ensure($filename);
+		preg_match_all("/^(.+?)\|/m", $content, $matches);
+		
+		return array_unique($matches[1]);
+	}
+	
+	/**
+	 * 
+	 * @param string[] $filenames
+	 * @return string[]
+	 */
+	public static function get_all_gallery_names($filenames) {
+		
+		$tmp_file_name = TMP_DIRECTORY_SLASH . self::TEMP_FILE_ALL_GALLERIES_CACHE;
+		$tmp_file_contents = file_get_contents($tmp_file_name);
+
+		//read from temp file
+		if ($tmp_file_contents) {
+			$cached_content = unserialize($tmp_file_contents);
+			$cached_filenames = $cached_content["filenames"];
+			
+			if ($filenames === $cached_filenames) {
+				return $cached_content["galleries"];
+			}
+		}
+		
+		$content = array_unique(
+			array_merge_all(
+				array_map_filter(str_prepend($filenames, $log_directory), 
+					[self::class, "get_all_gallery_names_by_file"])));
+		
+		$serialized_content = serialize(["filenames" => $filenames, "galleries" => $content]);
+		
+		file_put_contents_ensure($tmp_file_name, $serialized_content);
+		
+		return $content;
 	}
 	
 	/**
