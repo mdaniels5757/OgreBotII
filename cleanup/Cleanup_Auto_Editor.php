0e0f4c8c0a0b84bfee2860498bbe41db2f4419f7
Michael Daniels <mdaniels5757@gmail.com>
2020-04-15 15:07:55 -0400

    fix usernames in cleanup

--- cleanup/Cleanup_Auto_Editor.php
+++ cleanup/Cleanup_Auto_Editor.php
@@ -1,87 +1,87 @@
 <?php
 class Cleanup_Auto_Editor {
-	
+
 	/**
 	 *
 	 * @var Cleanup_Base
 	 */
 	private $cleanup_base;
-	
+
 	/**
 	 *
 	 * @var Page
 	 */
 	private $noref_pg;
-	
+
 	/**
 	 *
 	 * @var Page
 	 */
 	private $nootrs_pg;
-	
+
 	/**
 	 *
 	 * @var Page
 	 */
 	private $del_pg;
-	
+
 	/**
 	 *
 	 * @var Wiki
 	 */
 	private $co2;
-	
+
 	/**
 	 *
 	 * @var string
 	 */
 	private $edit_summary_all_files_desc;
-	
+
 	/**
 	 *
-	 * @param Wiki $co2        	
-	 * @param string $edit_summary_all_files_desc        	
+	 * @param Wiki $co2
+	 * @param string $edit_summary_all_files_desc
 	 */
 	public function __construct(Wiki $co2, $edit_summary_all_files_desc) {
 		global $logger, $wiki_interface;
-		
+
 		$logger->debug("Starting up...");
 		$this->cleanup_base = new Cleanup_Base();
-		
-		$this->noref_pg = $wiki_interface->new_page($co2, 
-			"User:OgreBot/Pages moved to Commons with missing information");
-		$this->nootrs_pg = $wiki_interface->new_page($co2, 
-			"User:OgreBot/Pages moved to Commons with missing OTRS templates");
-		$this->del_pg = $wiki_interface->new_page($co2, 
-			"User:OgreBot/Pages moved to Commons with possible licensing issues");
+
+		$this->noref_pg = $wiki_interface->new_page($co2,
+			"User:MDanielsBot/Pages moved to Commons with missing information");
+		$this->nootrs_pg = $wiki_interface->new_page($co2,
+			"User:MDanielsBot/Pages moved to Commons with missing OTRS templates");
+		$this->del_pg = $wiki_interface->new_page($co2,
+			"User:MDanielsBot/Pages moved to Commons with possible licensing issues");
 		$this->co2 = $co2;
 		$this->edit_summary_all_files_desc = $edit_summary_all_files_desc;
-		
+
 		$logger->debug("Startup finished");
 	}
-	
+
 	/**
 	 *
-	 * @param string $title        	
-	 * @param string $text        	
+	 * @param string $title
+	 * @param string $text
 	 * @param mixed $categories
 	 *        	currently unused
-	 * @param string $upload_date_millis        	
+	 * @param string $upload_date_millis
 	 * @return boolean
 	 */
 	public function process($title, $text, $categories, $upload_date_millis) {
 		global $logger, $wiki_interface;
-		
+
 		$made_change = false;
 		do {
 			$thistime = time();
 			$logger->debug("Autoprocessing $title");
-			$cleanup_response = $this->cleanup_base->super_cleanup($text, 
+			$cleanup_response = $this->cleanup_base->super_cleanup($text,
 				date("YmdHis", $upload_date_millis), false);
 			$newtext = $cleanup_response->get_text();
-			
+
 			$time_to_process = time() - $thistime;
-			
+
 			if ($time_to_process > 5) {
 				$logger->error("$title took $time_to_process seconds to process.");
 			}
@@ -89,7 +89,7 @@ class Cleanup_Auto_Editor {
 			if ($cleanup_response->get_significant_changes()) {
 				$editsummary = "clean up";
 			}
-			
+
 			if (0) { // !$categories) {
 				if (!$editsummary && preg_replace("/(?<!\r)\n/u", "\r\n", $text) !==
 					 preg_replace("/(?<!\r)\n/u", "\r\n", $newtext)) {
@@ -104,7 +104,7 @@ class Cleanup_Auto_Editor {
 					 "|month=" . date("F", $upload_date_millis) . "|day=" .
 					 date("j", $upload_date_millis) . "}}";
 			}
-			
+
 			if ($editsummary !== "") {
 				$pg = $wiki_interface->new_page($this->co2, $title);
 				if ($text !== $wiki_interface->get_page_text($pg, true)) {
@@ -115,21 +115,21 @@ class Cleanup_Auto_Editor {
 				}
 				$revisions = $pg->history(5000);
 				array_shift($revisions);
-				
+
 				$logger->info("Editing $title");
-				$wiki_interface->edit_suppress_exceptions($pg, $newtext, 
-					"(BOT): $editsummary of " . $this->edit_summary_all_files_desc, true, true, 
+				$wiki_interface->edit_suppress_exceptions($pg, $newtext,
+					"(BOT): $editsummary of " . $this->edit_summary_all_files_desc, true, true,
 					false, false, false);
 				$made_change = true;
 			}
 			$warnings = $cleanup_response->get_warnings();
-			
+
 			if ((array_remove($warnings, Cleanup_Shared::OTRS_REMOVED))) {
 				$logger->info("($title - logging no OTRS transclusion... ");
 				$newlink = "\n*[[:$title]]";
 				$text = $wiki_interface->get_page_text($this->nootrs_pg);
 				if (strpos($text, $newlink) === false) {
-					$wiki_interface->edit_suppress_exceptions($this->nootrs_pg, "\n*[[:$title]]", 
+					$wiki_interface->edit_suppress_exceptions($this->nootrs_pg, "\n*[[:$title]]",
 						"(BOT): adding [[$title]]", true, true, false, "ap");
 				} else {
 					$logger->info("already logged, skipped)");
@@ -137,11 +137,11 @@ class Cleanup_Auto_Editor {
 			}
 			if ((array_remove($warnings, Cleanup_Shared::LOCAL_DELETION))) {
 				$logger->info("($title - logging possibly deleteable transclusion... ");
-				
+
 				$newlink = "\n*[[:$title]]";
 				$text = $wiki_interface->get_page_text($this->del_pg);
 				if (strpos($text, $newlink) === false) {
-					$wiki_interface->edit_suppress_exceptions($this->del_pg, "\n*[[:$title]]", 
+					$wiki_interface->edit_suppress_exceptions($this->del_pg, "\n*[[:$title]]",
 						"(BOT): adding [[$title]]", true, true, false, "ap");
 				} else {
 					$logger->info("already logged, skipped)");
@@ -149,19 +149,19 @@ class Cleanup_Auto_Editor {
 			}
 			if ($warnings) {
 				$logger->info("($title - logging no cite replacement error)... ");
-				
+
 				$message = "\n*[[:$title]] - " .
 					 implode("; ", $cleanup_response->get_formatted_warnings(true));
 				$text = $wiki_interface->get_page_text($this->noref_pg);
 				if (strpos($text, $message) === false) {
-					$wiki_interface->edit_suppress_exceptions($this->noref_pg, $message, 
+					$wiki_interface->edit_suppress_exceptions($this->noref_pg, $message,
 						"(BOT): adding [[$title]]", true, true, false, "ap");
 				} else {
 					$logger->info("already logged, skipped)");
 				}
 			}
 		} while (0);
-		
+
 		return $made_change;
 	}
 }
--- cleanup/Do_Cleanup_Logic.php
+++ cleanup/Do_Cleanup_Logic.php
@@ -1,7 +1,7 @@
 <?php
 
 class Do_Cleanup_Logic {
-	
+
 	/**
 	 *
 	 * @param array $post_data.
@@ -9,11 +9,11 @@ class Do_Cleanup_Logic {
 	 *        	'image' => the file name to cleanup
 	 *        	'text' => the text for the filename. If not defined,
 	 *        	then the logic will fetch the text of the page.
-	 *        	
+	 *
 	 * @param callable $encodeLinkCallback
 	 *        	a callback which returns a string
 	 *        	when the link is passed to it.
-	 *        	
+	 *
 	 * @return array an array with the following indices
 	 *         'error" => (array of strings) if there was a retrival error. If there
 	 *         are no errors, this field will not be set.
@@ -29,26 +29,26 @@ class Do_Cleanup_Logic {
 	 *         * "major"
 	 *         * "minor"
 	 *         * "none"
-	 *        
+	 *
 	 */
 	public static function get_cleanup_data($post_data, $encodeLinkCallback) {
 		global $co, $logger, $messages, $validator, $wiki_interface;
-		
+
 		try {
 			$validator->validate_arg_array($post_data, "string");
 			$validator->validate_arg($encodeLinkCallback, "function");
-			
+
 			$logger->debug("get_cleanup_data");
 			$logger->debug($post_data);
 			$logger->debug($encodeLinkCallback);
-			
+
 			if (!array_key_exists('image', $post_data)) {
 				return array("error" => "No file name specified");
 			} else {
 				if ($co === null) {
-					$co = $wiki_interface->new_wiki("OgreBotCommons");
+					$co = $wiki_interface->new_wiki("MDanielsBotCommons");
 				}
-				
+
 				$title = $post_data['image'];
 				$image = $wiki_interface->new_image($co, $title);
 				$page = $image->get_page();
@@ -56,7 +56,7 @@ class Do_Cleanup_Logic {
 					$text = $post_data['text'];
 				} else {
 					$text = $wiki_interface->get_page_text($page);
-					$categories_query = $wiki_interface->query_pages($co, array($title), 
+					$categories_query = $wiki_interface->query_pages($co, array($title),
 						"categoriesnohidden");
 					$categories_query_thispage = array_pop($categories_query);
 					if (!array_key_exists('categories', $categories_query_thispage) ||
@@ -64,18 +64,18 @@ class Do_Cleanup_Logic {
 						$text .= "\r\n\r\n{{subst:unc}}";
 					}
 				}
-				
+
 				$history = $wiki_interface->get_upload_history($image);
-				if (count($history) > 0 && preg_match("/^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z$/", 
+				if (count($history) > 0 && preg_match("/^\d{4}\-\d{2}\-\d{2}T\d{2}:\d{2}:\d{2}Z$/",
 					$history[count($history) - 1]['timestamp'], $earliest_timestamp)) {
-					
-					$cleanup_response = (new Cleanup_Base())->super_cleanup($text, 
+
+					$cleanup_response = (new Cleanup_Base())->super_cleanup($text,
 						date("YmdHis", strtotime($earliest_timestamp[0])), TRUE);
-					
+
 					$cleanup_text = $cleanup_response->get_text();
-					
+
 					$warnings = $cleanup_response->get_formatted_warnings(false);
-					
+
 					if ($cleanup_response->get_significant_changes()) {
 						$changes = "major";
 					} else {
@@ -84,12 +84,12 @@ class Do_Cleanup_Logic {
 						$changes = $any_changes ? "minor" : "none";
 					}
 					$edit_time = preg_replace(
-						"/^(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/", "$1$2$3$4$5$6", 
+						"/^(\d{4})\-(\d{2})\-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/", "$1$2$3$4$5$6",
 						$page->get_lastedit());
-					
+
 					$default_summary = array_key_or_exception($messages, 'do_cleanup.editsummary');
 					$summary = @$post_data['summary'];
-					
+
 					if ($changes !== "none") {
 						if ($summary) {
 							$summary = "$summary +$default_summary";
@@ -97,15 +97,15 @@ class Do_Cleanup_Logic {
 							$summary = $default_summary;
 						}
 					}
-					
-					return ["changes" => $changes, "warnings" => $warnings, 
+
+					return ["changes" => $changes, "warnings" => $warnings,
 						"text" => $cleanup_text, "edittime" => $edit_time, "summary" => $summary];
 				} else {
 					$encodedTitle = $encodeLinkCallback($title);
 					$validator->validate_arg($encodedTitle, "string");
-					
+
 					return [
-						"error" => replace_named_variables("\$title not found!", 
+						"error" => replace_named_variables("\$title not found!",
 							["title" => $encodedTitle])];
 				}
 			}
