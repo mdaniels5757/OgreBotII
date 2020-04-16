3c0ac611d5ce483389438737ade928a661aec5b0
Michael Daniels <mdaniels5757@gmail.com>
2020-04-15 23:39:49 -0400

    fix error?

--- cron/cron-date-factory/File_System_Cron_Date_Factory.php
+++ cron/cron-date-factory/File_System_Cron_Date_Factory.php
@@ -1,39 +1,39 @@
 <?php
 
 /**
- * 
+ *
  * @author magog
  *
  */
 class File_System_Cron_Date_Factory extends Cron_Date_Factory {
-	
+
 	/**
-	 * 
+	 *
 	 * @var string
 	 */
 	const PROCESS_FILE_DIRECTORY = BASE_DIRECTORY;
-	
+
 	/**
-	 * 
+	 *
 	 * @var string
 	 */
 	const PROCESS_FILE_NAME = "process.txt";
 
 	/**
-	 * 
+	 *
 	 * @var int Unix timestamp
 	 */
 	private $now;
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @return string
 	 */
 	private static function get_process_file_name() {
 		return self::PROCESS_FILE_DIRECTORY . "/" . self::PROCESS_FILE_NAME;
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 *
@@ -41,10 +41,11 @@ class File_System_Cron_Date_Factory extends Cron_Date_Factory {
 	 */
 	protected function get_range() {
 		global $logger;
-		
+
 		// get next run time.
 		$processRunFileName = self::get_process_file_name();
-		
+		$interval = $this->get_interval();
+        
 		$this->now = time();
 		if (!filesize($processRunFileName)) {
 			$logger->warn("$processRunFileName doesn't exist. Attempting to create it.");
@@ -54,18 +55,18 @@ class File_System_Cron_Date_Factory extends Cron_Date_Factory {
 			$processRunFileContents = file_get_contents($processRunFileName);
 			$logger->debug("Process file content: $processRunFileContents");
 			$previousTime = intval($processRunFileContents);
-			
+
 			$logger->debug(
 				"Previous run time: $previousTime: " . date('Y-m-d H:i:s', $previousTime));
-			
+
 			if ($previousTime <= 0) {
 				throw new IllegalStateException("Illegal previous run time: $processRunFileContents");
 			}
 		}
-		
+
 		return [$previousTime + $this->get_interval() * SECONDS_PER_HOUR, $this->now];
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 *
